ae30d020dda96ff7ddfb59a769f45f0ffe4135b6
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-19 23:48:52 -0500

    playing with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"], //use this for dev -- make sure to move back to docs folder
   theme: {
     extend: {
       fontFamily: {
